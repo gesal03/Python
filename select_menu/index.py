96d46add9a9088c00a9bcf028afbe36428080aeb
이현승 <77336664+gesal03@users.noreply.github.com>
2022-04-24 18:45:56 +0900

    ver 2
    list 요소 set으로 변환

--- select_menu/index.py
+++ select_menu/index.py
@@ -1,7 +1,7 @@
 import random
 import time
 
-menu = ["된장찌개", "피자", "제육볶음", "짜장면"]
+menu = set(["된장찌개", "피자", "제육볶음", "짜장면"])
 
 
 while True:
@@ -9,21 +9,21 @@ while True:
     want_menu = input("음식을 추가 해주세요(끝내기 : q): ")
     if want_menu == 'q':
         break
-    menu.append(want_menu)
+    menu |= set([want_menu])
 
 while True:
     print("\n점심 메뉴 후보: ", menu)
     del_menu = input("음식을 삭제해주세요(끝내기 : q): ")
     if del_menu == 'q':
         break
-    menu.remove(del_menu)
+    menu -= set([del_menu])
 
 print("=============================================================")
 print(menu, "중에서 선택합니다 !!\n")
 for i in range(1,6):
     print(6-i)
     time.sleep(1)
-lunch = random.choice(menu)
+lunch = random.choice(list(menu))
 print("오늟의 메뉴는", lunch, "이다!\n")
 
 

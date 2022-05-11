645276421deef679d863bcd368419f299b03f6d5
이현승 <77336664+gesal03@users.noreply.github.com>
2022-05-11 18:18:21 +0900

    ver 2
    d

--- crawling/index.py
+++ crawling/index.py
@@ -9,8 +9,15 @@ rank = 1
 
 results = soup.findAll('b')
 
+favorsch = open("rankresult.txt", "w")
+
 print(datetime.today().strftime("%Y년 %m월 %d일의 실시간 검색어 순위입니다.\n"))
 
 for result in results:
+    if rank > 10 :
+        break
+    favorsch.write(str(rank) + "위 " + result.get_text() + "\n")
     print(rank,"위 : ",result.get_text(),"\n")
     rank += 1
+
+favorsch.close() 
